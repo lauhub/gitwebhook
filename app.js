51d2b0f7d124204e908e7c89614bd6318bc44375
Laurent HUBERT <lau.hub@gmail.com>
2019-01-22 09:00:16 +0000

    Working prototype

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+/*******************************************************************************
+ * A Webhook script for github
+ *
+ * Author: Laurent HUBERT
+ * License: GPL
+ *
+ * Dependencies and source code re-use:
+ * 
+ * shelljs : Copyright (c) 2012, Artur Adib <arturadib@gmail.com>
+ * All rights reserved.
+ * 
+ *
+ ******************************************************************************/
+
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var morgan = require('morgan');
+var shell = require('shelljs');
 
 var app = express();
 
@@ -10,10 +25,51 @@ var app = express();
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 app.use(morgan('short', { stream: accessLogStream }))
 
+var gitRepos = require('./gitFunctions');
+
+gitRepos.checkGitInstalled();
+
+//var directory = path.join(__dirname, 'voting-app') ;
+
+var directory = '/home/lauhub';
+
+function createCallback(resolve, reject, next, callback){
+	return function(code, stdout, stderr){
+		return callback(code, resolve, reject, next);
+	};
+}
+
+var obj = JSON.parse(fs.readFileSync('gitwebhook.conf', 'utf8'));
+
 app.get('/', function(req, res) {
-  res.send('salut les poulets !');
+	if(gitRepos.setReposDir(directory) != 0){
+		res.send('Directory '+directory+' does not exist');
+	}
+	else {
+		var p = new Promise((resolve, reject) => {
+				gitRepos.pullFromOrigin((code, stdout, stderr) => {
+					if(code != 0){
+						reject();
+					}
+					else{
+						resolve(true);
+					}
+				});
+		});
+		p.then(successful => {
+        if (successful) {
+          res.send('done');
+          //  response.redirect('/connected');
+        }
+        else {
+					res.send('failed');
+          //  response.redirect('/failure');
+        }
+    });
+    p.catch(() => {res.send('exception');});
+	}
 });
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('listening on port 3000');
 });
---
+++ gitFunctions.js
@@ -0,0 +1,23 @@
+module.exports = {
+	shell: require('shelljs'),
+  checkGitInstalled: function () {
+    if (!this.shell.which('git')) {
+			this.shell.echo('Sorry, this script requires git');
+			this.shell.exit(1);
+			return false;
+		}
+		else {
+			console.log("git found !");
+			return true;
+		}
+  },
+  setReposDir: function (newDir, callback) {
+  	this.directory = newDir;
+    return this.shell.cd(this.directory).code;
+  },
+  pullFromOrigin: function (callback) {
+  	console.log("Executing pullFromOrigin");
+    this.shell.exec('git pull', callback);
+  	console.log("End of pullFromOrigin");
+  }
+};
