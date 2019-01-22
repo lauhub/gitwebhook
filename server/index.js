4ffd7391b04985205ee0204cd224f0f2fd3b6bc6
Laurent HUBERT <lau.hub@gmail.com>
2019-01-22 11:57:18 +0000

    Refactoring of git-functions

--- server/index.js
+++ server/index.js
@@ -2,31 +2,23 @@ module.exports = function(app, configuration, gitRepos){
 	var shell = require('shelljs');
 	
 	app.get('/', function(req, res) {
-		if(gitRepos.setReposDir(__dirname) != 0){
-			res.send('Directory '+ __dirname +' does not exist');
-		}
-		else {
-			var p = new Promise((resolve, reject) => {
-					gitRepos.pullFromOrigin((code, stdout, stderr) => {
-						if(code != 0){
-							reject(new Error("code : "+ stdout+ "stderr:\n"+stderr));
+			res.send('failed : unknown request handler');
+	});
+	
+	configuration.hooks.forEach(function(hook){
+		console.log(hook);
+		console.log(`Adding hook ${hook.id}`);
+		app.get('/'+hook.id, function(req, res) {
+				gitRepos.createRequestHandlerPromise(hook.dir, hook.cmd, hook.id)
+				.then(successful => {
+						if (successful) {
+							res.send('done ' + successful);
 						}
-						else{
-							resolve(true);
+						else {
+							res.send('failed '+successful);
 						}
-					});
-			});
-			p.then(successful => {
-					if (successful) {
-						res.send('done');
-						//  response.redirect('/connected');
-					}
-					else {
-						res.send('failed');
-						//  response.redirect('/failure');
-					}
-			}).catch(error => {res.send('exception: '+error.message);});
-		}
+				}).catch(error => {res.send('exception: '+error.message);});
+		});
 	});
 	
 	app.listen(3000, function() {
